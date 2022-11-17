9780823039d7af8a918e849d71280a47b293c70b
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2022-11-17 09:11:55 -0300

    Coffee Select Ok

--- src/components/CardCoffee/index.tsx
+++ src/components/CardCoffee/index.tsx
@@ -1,26 +1,44 @@
 import React, { useContext, useState } from 'react'
-import { Buy, BuyContainer, BuyDivStyled, DivStyled, ItemContainer } from './styles'
+import { Buy, BuyContainer, BuyDivStyled, ControlesBuy, DivStyled, ItemContainer, SpanStyles, SpanStyles02 } from './styles'
 import ShoppingCartSimple from '../../assets/ShoppingCartSimple.png'
 import { Tags } from '../tags'
 import { Coffee, ContextCoffee } from '../../context/context'
 import { actions } from '../../redurcers/redurcer'
-import { Controles } from '../ControlesBuy'
 
 
 export function CardCoffee(item: Coffee) {
-
     const { dispatch, state } = useContext(ContextCoffee)
-    const CoffeeSelect: Coffee = {
-        amount: state.counter,
-        buy: item.buy,
-        description: item.description,
-        img: item.img,
-        name: item.name,
-        tags: item.tags,
+    const [counter, setCounter] = useState(1)
+
+    function handleCounter(value: number) {
+        const newValue = counter + value
+        if (newValue < 1 || newValue > 10) { return }
+
+        setCounter(newValue)
+
     }
 
     function submit() {
-        dispatch({ type: actions.submitCard, playload: CoffeeSelect })
+        const CoffeeSelect: Coffee = {
+            id: item.id,
+            amount: counter,
+            buy: item.buy,
+            description: item.description,
+            img: item.img,
+            name: item.name,
+            tags: item.tags,
+        }
+        if (state.CoffeeSelect.length > 0) {
+
+            const CoffeeSelectName = state.CoffeeSelect.some((item) => (item.name === CoffeeSelect.name))
+
+            if (CoffeeSelectName) { dispatch({ type: actions.submitAmount, playload: { id: item.id, counter: counter } }) }
+            else { { dispatch({ type: actions.submitCard, playload: CoffeeSelect }) } }
+
+        } else {
+            dispatch({ type: actions.submitCard, playload: CoffeeSelect })
+        }
+
     }
     return (
 
@@ -35,11 +53,15 @@ export function CardCoffee(item: Coffee) {
             <BuyContainer>
                 <p>R$
                     <span>
-                        {`${item.buy.toString().replace('.', ',')}0`}
+                        {`${item.buy.toFixed(2).replace('.', ',')}`}
                     </span>
                 </p>
                 <BuyDivStyled >
-                    <Controles />
+                    <ControlesBuy>
+                        <SpanStyles onClick={() => { handleCounter(-1) }}>-</SpanStyles>
+                        <span>{counter}</span>
+                        <SpanStyles02 onClick={() => { handleCounter(1) }}>+</SpanStyles02>
+                    </ControlesBuy>
                     <Buy onClick={() => { submit() }}>
                         <img src={ShoppingCartSimple} />
                     </Buy>
